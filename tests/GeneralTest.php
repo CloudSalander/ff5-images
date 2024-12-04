24099d9140e2a6a12bbc5cf09545c1f07999180e
CloudSalander <ruben.as532@gmail.com>
2024-12-04 18:01:25 +0100

    Solving 404 API response

--- tests/GeneralTest.php
+++ tests/GeneralTest.php
@@ -6,14 +6,9 @@ class GeneralTest extends BaseTest {
     
     public function test404NotFound()
     {
-        $wrong_uri_sample = 'image';
-        $this->client->post($wrong_uri_sample);
+        $wrong_uri_sample = 'images';
+        $response = $this->client->post($wrong_uri_sample);
        
-        $response = $e->getResponse();
         $this->assertEquals(404, $response->getStatusCode());
-
-        $data = assertJSONResponse($response);
-        
-        $this->assertResponseContent($data,'Endpoint Not found',-1);
     }
 }
