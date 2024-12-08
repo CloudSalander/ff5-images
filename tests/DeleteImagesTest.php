ccc823fd7a73f3fe338b14088153d0f4cc64cf1b
CloudSalander <ruben.as532@gmail.com>
2024-12-08 02:40:41 +0100

    Added delete image test

---
+++ tests/DeleteImagesTest.php
@@ -0,0 +1,25 @@
+<?php
+
+class DeleteImagesTest extends BaseTest
+{
+    public function testDeleteNonExistingImage() {
+        $this->clearImagesTable();
+        $id = 1;
+        $response = $this->client->delete("images/{$id}");
+        
+        $this->assertEquals(404, $response->getStatusCode());
+        $data = $this->assertJSONResponse($response->getBody());
+        $this->assertResponseContent($data,'No images found!',9);
+    }
+
+    public function testDeleteExistingImage() {
+        $this->clearImagesTable();
+        $this->insertImages(1);
+        $id = 1;
+        $response = $this->client->delete("images/{$id}"); 
+        
+        $this->assertEquals(200, $response->getStatusCode());
+        $data = $this->assertJSONResponse($response->getBody());
+        $this->assertResponseContent($data,'Successful operation',1);
+    }
+}
